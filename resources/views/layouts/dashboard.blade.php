bef5c0a43a60b87fdae1d075c4293b310f03f69c
wijoyo wisnu mukti <wisnu.mukti10@gmail.com>
2021-08-13 11:51:45 +0700

    fixed logo

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -62,10 +62,10 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{route('home')}}" class="brand-link">
+    <span class="brand-link">
       <img src="{{asset('template/dist/img/yaksa/Logo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Yaksa Harmoni Global</span>
-    </a>
+    </span>
 
     <!-- Sidebar -->
     <div class="sidebar">
